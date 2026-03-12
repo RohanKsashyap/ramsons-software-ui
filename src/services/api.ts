3171c2f0a3abfdc81e2b7b132247603067d11c05
RohanKashyap <roy282227@gmail.com>
2026-03-12 23:17:02 +0530

    commit the changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,8 +14,9 @@ interface Customer {
   advancePayment?: number;
 }
 
- const API_BASE_URL = 'https://ramsons-software-backend.onrender.com/api/v1';
+//  const API_BASE_URL = 'https://ramsons-software-backend.onrender.com/api/v1';
 //const API_BASE_URL = 'http://localhost:5000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 class ApiService {
   private token: string | null = null;
